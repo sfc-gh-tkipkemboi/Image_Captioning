6f94f7edd3389afa3b4862a3dfec1a58025df7a6
Tony Kipkemboi <117667118+sfc-gh-tkipkemboi@users.noreply.github.com>
2024-01-08 12:15:51 -0500

    Update streamlit.py

--- code/streamlit.py
+++ code/streamlit.py
@@ -16,11 +16,11 @@ from gtts import gTTS
 from IPython.display import Audio
 
 #caption_model_vgg = keras.models.load_model('../models/caption_mode_with_vgg16.h5')
-model_path = Path('../models/caption_mode_with_vgg16.h5')
+model_path = '../models/caption_mode_with_vgg16.h5'
 caption_model_vgg = keras.models.load_model(model_path)
 
 #df = pd.read_csv('../data/cleaned_caption.csv')
-csv_file_path = Path('../data/cleaned_caption.csv')
+csv_file_path = '../data/cleaned_caption.csv'
 df = pd.read_csv(csv_file_path)
 
 # Title, head and 
